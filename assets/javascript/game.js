168ece73ae26214e4ef69034ae7019a8cd8da8a2
Cat <madcatmac16@gmail.com>
2018-08-12 01:19:51 -0600

    more push

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,46 +1,64 @@
-/*first call out the variables/game object */
-/*var playerWins = "win";
-var hiddenWord = "word";
-var totalGuesses = "guesses";
-var boneYard = "yard";
-*/
-var game = {
-    playerWins = "win",
-    hiddenWord = "word",
-    totalGuesses = "guesses",
-    boneYard = "yard",
-    words = ['sid', 
-    'rex', 
-    'andy', 
-    'woody', 
-    'buzz', 
-    'aliens', 
-    'slinky', 
-    'little bo peep',
-    'hamm',
-    'sergeant',
-    'shark', 
-    'troll'],
-    letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "_"]
 
+    var word;
+    var allowedGuesses;
+    var guessWord;
+    var wrongGuesses;
+
+    var wordElement = document.getElementById('word');
+    var letterCountElement = document.getElementById('letterCount');
+    var lettersGuessedElement = document.getElementById('lettersGuessed');
+
+    var words = ["sid", 
+    "rex", 
+    "andy", 
+    "woody", 
+    "buzz", 
+    "aliens", 
+    "slinky", 
+    "little bo peep",
+    "hamm",
+    "sergeant",
+    "shark", 
+    "troll"];
+    var letters = ["abcdefghijklmnopqrstuvwyxz"];
+    
+
+// computer random picking from "word" array
+    var computerPick = words[Math.floor(Math.random()*words.length)];
+    var guessWord = new Array(computerPick.length);
+
+    function initializeGame() {
+// computer random picking from "word" array
+        var computerPick = words[Math.floor(Math.random()*words.length)];
+        var guessWord = new Array(computerPick.length);
+        allowedGuesses = 25;
+        correctGuess = [];
+        boneYard = [];
+        
+// empty variable computer guess with _ by for loop*/
+    for (var i = 0; i < guessWord.length; i++) {
+    guessWord.push('_');
+    }
+
+// my console for allowedGuesses and  guessWord
+    wordElement.innerHTML = guessWord.join('_');
+    letterCountElement.innerHTML = allowedGuesses;
 }
 
-/*var wins = 12;
-var words = ['sid', 'rex', 'andy', 'woody', 'buzz', 'aliens', 'slinky', 'little bo peep', 'hamm','sergeant','shark', 'troll']
-var word = chooseWord(words);
-var guesses =new set([]);
-var game_obj = game.Game(win, word, guesses);
-var letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "_"];
-$(document).keypress(function(i) {
-    handleInput(i);
-});
-var handleInput =function(i) {
-    if (!isLetter(e.key) || (game_obj.guesses.has(e.key))) {
-        return;
-    }
-    game_obj = game_obj.addGuess(e.key);
-    scr = screen.Screen(game_obj);
-    drawScreen(scr);
-}*/
+//print hidden word by _ length
+    document.getElementById("word").innerHTML = guessWord;
+    
+//update guesses
+    function updateGuesses(letter) {
+
+}
+//compare the letter to the letters in the hidden computer pick    
+//create event listener for one up key
+    document.onkeyup = function (event) {
+        var lettersGuessed =String.fromCharCode(event.keyCode).toLocaleLowerCase();
+        updateGuesses(lettersGuessed);
+        checkWin();
+    };
+
 
 var Game = function()
