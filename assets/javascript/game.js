b1b8fae64c94bde4e0e33ec3586f12da5cddc89a
Cat <madcatmac16@gmail.com>
2018-08-12 18:20:25 -0600

    mostofjsandcss

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -13,26 +13,26 @@ var words = ["sid",
 var game = {
   guessed: [],
   left: 15,
-  
+// computer random picking from "word" array and empty variable computer guess with _ by for loop 
   start: function() {
     this.complete = false;
     this.word = words[Math.floor(Math.random() * words.length)];
-    this.$right = document.getElementById('worddiv');
+    this.$right = document.getElementById('right');
     this.$wrong = document.getElementById('yard');
     this.$remain = document.getElementById('remain');
-    this.$right.innerHTML = '_'.repeat(this.word.length);
+    this.$right.innerHTML = ' _ '.repeat(this.word.length);
   },
-  
+//compare the letter to the letters in the hidden computer pick   
   guess: function(letter) {
     if (this.left > 0 && this.complete != true) {
-      if (this.word.indexOf(letter) > -1 || this.guessed.indexOf(letter) > -1) {
+      if (this.word.indexOf(letter) > 0 || this.guessed.indexOf(letter) > 0) {
         this.right(letter);
       } else {
         this.wrong(letter);
       }
     }
   },
-  
+ //ERROR fix the replace of letter to  _ index to win game  
   right: function(letter) {
     for(var i = 0; i < this.word.length; i++) {
       if (this.word[i] == letter) {
@@ -41,7 +41,7 @@ var game = {
         this.$right.innerHTML = word.join('');
       }
     }
-    if (this.$right.innerHTML.indexOf(' _ ') < 0) {
+    if (this.$right.innerHTML.indexOf('_') < 0) {
       alert('you win!');
       this.complete = true;
     }
@@ -58,9 +58,13 @@ var game = {
     }
   }
 };
-
+//create event listener for one up key
 game.start();
 document.onkeyup = function(event) {
   var letter = String.fromCharCode(event.keyCode).toLowerCase();
   game.guess(letter);
 };    
+//auto play again 
+//$(document).ready(function(document) {
+   // $('#playAgain').click(document);
+    //});
